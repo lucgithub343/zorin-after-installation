41c246cfa33e619abae0034fa4e16a8a400489f0
lucgithub343 <luc.github@hotmail.com>
2024-03-19 13:57:41 -0300

    retirando a

--- Zorin-17-Flatpak/after-installation-zorin-17-part-1.sh
+++ Zorin-17-Flatpak/after-installation-zorin-17-part-1.sh
@@ -998,7 +998,7 @@ sudo apt update -y
 sudo apt install --target-release 'o=LP-PPA-mozillateam' firefox -y
 
 echo "Package: firefox*" | sudo tee -a /etc/apt/preferences.d/mozillateamppa
-echo "Pin: release o=LP-PPA-mozillateam" | sudo tee -a /etc/apt/preferences.d/mozillateamppaa
+echo "Pin: release o=LP-PPA-mozillateam" | sudo tee -a /etc/apt/preferences.d/mozillateamppa
 echo "Pin-Priority: 501" | sudo tee -a /etc/apt/preferences.d/mozillateamppa
 
 sudo apt update -y
