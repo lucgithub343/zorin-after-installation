34b18184e332d780b74439fae3ab2c12c9416e39
lucgithub343 <luc.github@hotmail.com>
2024-06-22 21:41:15 -0300

    removendo a remocao do kernel 6.5

--- Nvidia/01-reinstall-kernel-6-2.sh
+++ Nvidia/01-reinstall-kernel-6-2.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-
-sudo apt remove --purge --yes linux-headers-6.5.0-14-generic linux-image-6.5.0-14-generic linux-modules-6.5.0-14-generic linux-modules-extra-6.5.0-14-generic
-sudo apt clean -y
-sudo apt autoremove -y
-
 sudo apt install --reinstall --yes linux-headers-6.2.0-39-generic linux-image-6.2.0-39-generic linux-modules-6.2.0-39-generic linux-modules-extra-6.2.0-39-generic
-sudo apt-mark hold linux-headers-6.2.0-39-generic linux-image-6.2.0-39-generic linux-modules-6.2.0-39-generic linux-modules-extra-6.2.0-39-generic 
+sudo apt-mark hold linux-headers-6.2.0-39-generic linux-image-6.2.0-39-generic linux-modules-6.2.0-39-generic linux-modules-extra-6.2.0-39-generic
 
 sudo apt clean -y
 sudo apt autoremove -y
