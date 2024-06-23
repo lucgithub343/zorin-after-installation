1423cff239da6765a0ac48c87c0d172dd51023bc
lucgithub343 <luc.github@hotmail.com>
2024-06-23 17:37:03 -0300

    instalando navegador Midori

--- Zorin-17/after-installation-zorin-17-part-1.sh
+++ Zorin-17/after-installation-zorin-17-part-1.sh
@@ -911,6 +911,12 @@ sudo apt install umbrello -y
 ## Instalando VirtualBox
 sudo apt install virtualbox -y
 
+## Instalando Browser Midori
+sudo wget -O /etc/apt/trusted.gpg.d/midori-archive-keyring.gpg http://repo.astian.org/midori-archive-keyring.gpg
+echo "deb http://repo.astian.org midori main" | sudo tee /etc/apt/sources.list.d/midori.list
+sudo apt update -y
+sudo apt install midori -y
+
 ## Instalando o modulo "libcanberra-gtk-module" para que nao ocorra o erro: “failed to load module canberra-gtk-module”
 sudo apt install libcanberra-gtk-module libcanberra-gtk3-module -y
 
