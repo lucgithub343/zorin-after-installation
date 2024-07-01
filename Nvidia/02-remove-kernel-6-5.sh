47a266dbabce9e83424489f29ba79242e597f32f
lucgithub343 <luc.github@hotmail.com>
2024-07-01 05:26:49 -0300

    remocao de um Kernel a mais que aparece depois de remover todos

--- Nvidia/02-remove-kernel-6-5.sh
+++ Nvidia/02-remove-kernel-6-5.sh
@@ -7,6 +7,7 @@ sudo apt purge linux-image-generic-hwe-22.04 -y
 sudo apt purge linux-image-6.5.0-27-generic -y
 sudo apt purge linux-image-6.5.0-35-generic -y
 sudo apt purge linux-image-6.5.0-41-generic -y
+sudo apt purge linux-image-unsigned-6.5.0-41-generic -y
 
 sudo apt clean -y
 sudo apt autoremove -y
