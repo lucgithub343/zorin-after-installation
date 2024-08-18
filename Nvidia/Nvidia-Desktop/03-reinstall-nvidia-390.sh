7ca01ada94dfda3bb0eb42b17a93629f2f08a7e5
lucgithub343 <luc.github@hotmail.com>
2024-08-18 17:01:48 -0300

    mudando a maneira de instalar a placa de video

--- Nvidia/Nvidia-Desktop/03-reinstall-nvidia-390.sh
+++ Nvidia/Nvidia-Desktop/03-reinstall-nvidia-390.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-dpkg -l | grep linux-image
+sudo apt autoremove nvidia* --purge -y
+
+sudo apt install nvidia-cuda-toolkit -y
+
+## Download CUDA-CUDNN
+if [ -e cudnn-local-repo-ubuntu2204-9.3.0_1.0-1_amd64.deb ];
+then
+    echo "O arquivo  cudnn-local-repo-ubuntu2204-9.3.0_1.0-1_amd64.deb  ja existe"
+else
+    echo -e "\n\n\n\n CUDA-CUDNN"
+    wget https://github.com/lucgithub343/placa-video-GT-610/releases/download/CUDA-CUDNN/cudnn-local-repo-ubuntu2204-9.3.0_1.0-1_amd64.deb
+fi
+
+
+chmod +x cudnn-local-repo-ubuntu2204-9.3.0_1.0-1_amd64.deb
+sudo dpkg -i cudnn-local-repo-ubuntu2204-9.3.0_1.0-1_amd64.deb
+
+
+sudo cp /var/cudnn-local-repo-ubuntu2204-9.3.0/cudnn-*-keyring.gpg /usr/share/keyrings/
+
 
 sudo apt update -y
-sudo ubuntu-drivers devices
-sudo apt install -y nvidia-driver-390
+sudo apt install cudnn-cuda-12 -y
+sudo apt install nvidia-driver-390 -y
